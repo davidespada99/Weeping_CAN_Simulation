57742c98359410cdb2bf8a00f799767100c66984
mizzet1 <64850100+mizzet1@users.noreply.github.com>
2024-12-09 15:11:19 +0100

    Delete generateRandomMessage.m

--- generateRandomMessage.m
+++
@@ -1,9 +0,0 @@
-function [randomMsg] = generateRandomMessage()
-    % Generate random CAN message
-    fixedID = 100; % Random standard CAN ID (11 bits, range: 1-2047)
-    randData = uint8(randi([0, 255], 1, 8)); % Random data for DLC length
-    
-    % Create the random CAN message
-    randomMsg = canMessage(fixedID, false, 8);
-    randomMsg.Data = randData;
-end
