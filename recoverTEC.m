e73629a707ecbb2ea0fd88a67f3e874eedfc354e
mizzet1 <64850100+mizzet1@users.noreply.github.com>
2024-12-10 16:47:30 +0100

    Update recoverTEC.m

--- recoverTEC.m
+++ recoverTEC.m
@@ -1,10 +1,15 @@
 % Function to handle TEC recovery and adjustments
 function [tecA, tecV] = recoverTEC(tecA, tecV, victim, victimMsg, attacker, attackRatio)
-    
+    % Perform recovery actions only if the victim or the attacker has not entered bus off
+    % state
+    if(tecV>255 || tecA>255)
+        return;
+    end
     % Now the victim retransmit the message successfully --> decrement tecV
     transmit(victim, victimMsg);
     tecV = tecV - 1; 
-
+    
+   
     % Attacker sends additional messages and recovers
     for i = 1 : attackRatio
         transmit(attacker, generateRandomMessage());
